de571f1d441dcf5aeefc7ee2ed339f17be6397c0
王能才 <nengcai.wang@ucarinc.com>
2018-08-23 10:49:09 +0800

    删除dmm

--- src/com/wnc/mymoney/ui/MainActivity.java
+++ src/com/wnc/mymoney/ui/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends Activity implements UncaughtExceptionHandler
         initComponents();
         setViewsIfChange();
         
-        DmmSpider.run();
+//        DmmSpider.run();
         
     }
 
